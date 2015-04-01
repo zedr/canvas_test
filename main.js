fd6c86ca5d5d281fe4b62d8a69984d0a07b95c3a
Rigel Di Scala <rigel.discala@propylon.com>
2015-04-01 18:19:50 +0100

    Refactor the main script

--- main.js
+++ main.js
@@ -2,10 +2,12 @@
   "use strict";
 
   NS.onload = function () {
-    NS.gameApp = new NS.Resources.Game();
-    NS.gameApp.setCanvas(NS.document.getElementById("target"));
-    NS.gameApp.bindController("mouse");
-    NS.gameApp.start();
+    var gameApp = new NS.Resources.Game(),
+      canvas = NS.document.getElementById("target");
+
+    gameApp.setCanvas(canvas);
+    gameApp.bindController("mouse");
+    gameApp.start();
   }
 
 }(this));
